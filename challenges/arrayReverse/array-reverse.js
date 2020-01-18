0f9331657946a8d530e9f4e87ef38de6ab35fb39
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2020-01-18 09:37:36 +0200

    a working reversing algorithm

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -1,9 +1,13 @@
 'use strict'
 
+// let array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
+
 const reverseArray = (arr) => {
     let reversedArray = [];
-    for (let i= arr.length; i>=0; i--){
-        reversedArray[reversedArray.length]=arr[i];
+    for (let i= array.length-1; i>=0; i--){
+        reversedArray[reversedArray.length]=array[i];
     };
     return reversedArray;
 };
+
+// console.log(reverseArray());
