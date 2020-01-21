024341d482b1c87489c742c0e769407baadac0c0
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2020-01-21 21:57:20 +0200

    added the needed documentaion

--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,6 +1,12 @@
 'use strict';
 let arrayBinarySearch = module.exports = {};
 
+/**
+ * the function checks if the key is located in the array and returns the value of the index
+ * @param arr
+ * @param key
+ * @return {boolean}
+ */
 arrayBinarySearch.indexNum = (arr, key) => {
   let index =  0;
   for (let i=0; i<arr.length ; i++){
